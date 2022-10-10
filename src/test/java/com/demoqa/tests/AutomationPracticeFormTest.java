805116d413b40a732b47d862735ac6dbb0742f5e
KOELN\A17877C <jan.largin@rewe-group.com>
2022-10-10 17:18:59 +0200

    Moving selectors in the variables, user input data generation with Faker

--- src/test/java/com/demoqa/tests/AutomationPracticeFormTest.java
+++ src/test/java/com/demoqa/tests/AutomationPracticeFormTest.java
@@ -3,6 +3,7 @@ package com.demoqa.tests;
 import com.codeborne.selenide.Configuration;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -10,7 +11,9 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 import java.io.File;
 
@@ -20,12 +23,15 @@ public class AutomationPracticeFormTest {
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
     }
 
+    @Disabled
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
         $("#firstName").setValue("Ivan");
         $("#lastName").setValue("Petrov");
         $("#userEmail").setValue("Ivan.Petrov@test.com");
@@ -34,10 +40,10 @@ public class AutomationPracticeFormTest {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("February");
         $(".react-datepicker__year-select").selectOption("1970");
-        $(".react-datepicker__day--001:not(.react-datepicker__day--selected").click();
+        $(".react-datepicker__day--001:not(.react-datepicker__day--selected)").click();
         $x("//input[@id='subjectsInput']").setValue("Arts").pressEnter();
         $(By.xpath("//div[@id='hobbiesWrapper']//label[contains(text(),'Music')]")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/Test.jpg"));
+        $("#uploadPicture").uploadFile(new File("src/test/resources/img/Test.jpg"));
         $("#currentAddress").setValue("Some address 1");
         $x("//input[@id='react-select-3-input']").setValue("NCR").pressEnter();
         $x("//input[@id='react-select-4-input']").setValue("Gurgaon").pressEnter();
@@ -50,7 +56,7 @@ public class AutomationPracticeFormTest {
                 text("01 February,1970"),
                 text("Arts"),
                 text("Music"),
-                text("img/Test.jpg"),
+                text("Test.jpg"),
                 text("Some address 1"),
                 text("NCR Gurgaon"));
         // $("#closeLargeModal").click();
--- src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTest.java
+++ src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTest.java
@@ -1,30 +1,36 @@
 package com.demoqa.tests;
 
+import static io.qameta.allure.Allure.step;
+
 import com.demoqa.pages.RegistrationFormPage;
-import com.demoqa.SetUp;
+import com.demoqa.BaseTest;
 import com.demoqa.testData.UserInformation;
 
 import org.junit.jupiter.api.Test;
 
-public class RegistrationFormWithPageObjectsTest extends SetUp {
+public class RegistrationFormWithPageObjectsTest extends BaseTest {
 
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
     @Test
     void fillFormTest() {
-        registrationFormPage.openPage()
-                .setFirstName(UserInformation.firstName)
-                .setLastName(UserInformation.lastName)
-                .setEmail(UserInformation.email)
-                .setGender(UserInformation.gender)
-                .setNumber(UserInformation.number)
-                .setBirthDate(UserInformation.day, UserInformation.month, UserInformation.year)
-                .setSubjects(UserInformation.subjects)
-                .setHobbies(UserInformation.hobbies)
-                .uploadFile(UserInformation.picturePath)
-                .setAddress(UserInformation.address, UserInformation.state, UserInformation.city)
-                .clickSubmit();
-
+        step("Open registrations form", () -> {
+        registrationFormPage.openPage();
+        });
+        step("Fill form", () -> {
+        registrationFormPage.setFirstName(UserInformation.firstName)
+                            .setLastName(UserInformation.lastName)
+                            .setEmail(UserInformation.email)
+                            .setGender(UserInformation.gender)
+                            .setNumber(UserInformation.number)
+                            .setBirthDate(UserInformation.day, UserInformation.month, UserInformation.year)
+                            .setSubjects(UserInformation.subjects)
+                            .setHobbies(UserInformation.hobbies)
+                            .uploadFile(UserInformation.picturePath)
+                            .setAddress(UserInformation.address, UserInformation.state, UserInformation.city)
+                            .clickSubmit();
+        });
+        step("Check form results", () -> {
         registrationFormPage.checkResultsTableVisible()
                 .checkResult("Student Name", UserInformation.firstName + " " + UserInformation.lastName)
                 .checkResult("Student Email", UserInformation.email)
@@ -36,7 +42,8 @@ public class RegistrationFormWithPageObjectsTest extends SetUp {
                 .checkResult("Picture", UserInformation.picture)
                 .checkResult("Address", UserInformation.address)
                 .checkResult("State and City", UserInformation.state + " " + UserInformation.city);
+                //.checkResult("State and City", UserInformation.state + " " + UserInformation.hobbies);
 
+        });
     }
-
 }
